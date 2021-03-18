9714592fa08b65337a28e83c06a2fabf4d9fe75d
CAMcDragon <62313742+CAMcDragon@users.noreply.github.com>
2021-03-18 19:34:47 -0400

    Create Entrypoint.cs

---
+++ BepInEx/BepInEx.Bootstrap/Entrypoint.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace BepInEx.Bootstrap
+{
+  public static class Entrypoint
+  {
+    public static void Init()
+    {
+      AppDomain.CurrentDomain.AssemblyResolve += ResolveBepInEx;
+      
+      Linker.StartBepInEx();
+    }
+    
+    public static readonly string LocalDirectory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+    
+    private static Assembly ResolveBepInEx(object sender, ResolveEventArgs args)
+    {
+      string path = Path.Combine(LocalDirectory, $@"BepInEx\core\{new AssemblyName(args.Name).Name}.dll");
+      
+      if(!File.Exists(path))
+      {
+        return null;
+      }
+      
+      try
+      {
+        return Assembly.LoadFile(path);
+      }
+      catch (Exception)
+      {
+        return null;
+      }
+    }
+  }
+}
